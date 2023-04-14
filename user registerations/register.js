ffa19c5e70df17dcbe2a96e2fbc03b0948ef1f3a
Sai Prakash <109500075+SaiPrakashSarisa@users.noreply.github.com>
2023-04-14 14:37:41 +0530

    Update register.js

--- user registerations/register.js
+++ user registerations/register.js
@@ -1,3 +1,27 @@
+var form = document.getElementById("myForm");
+const btn = document.getElementById("btn");
+
+// to know whether submit button is disabled or not
+console.log(`Is button has been disabled : ${btn.disabled}`);
+
+function validations() {
+  // logic for enabling button when all fields are filled.
+  if (form.checkValidity()) {
+    btn.disabled = false;
+
+    // to know whether submit button is disabled or not
+    console.log(`Is button has been disabled : ${btn.disabled}`);
+    btn.style.backgroundColor = "greenyellow";
+    btn.style.color = "black";
+  } else {
+    btn.disabled = true;
+    // to know whether submit button is disabled or not
+    console.log(`Is button has been disabled : ${btn.disabled}`);
+    btn.style.backgroundColor = "rgb(44, 44, 44)";
+    btn.style.color = "rgb(94, 94, 94)";
+  }
+}
+
 function valid() {
   const name = document.getElementById("userName").value;
   const mail = document.getElementById("mail").value;
